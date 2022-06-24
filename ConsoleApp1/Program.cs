eb1f995e8e255f141a7a7e7fc1c99aa3e56e82db
EstudianteMicrosoft <EstudianteMicrosoft@DESKTOP-M5I1S3J>
2022-06-24 13:30:46 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Al cerrar un expendio de naranjas, 15 clientes que aún no han pagado,
+  recibirán un 15% de descuento si compran más de 10 kilos. 
+  Determinar cuánto pagará cada cliente y cuanto percibirá la tienda por
+ esas compras.*/
+const int N = 5;
+int kilos, valorkilos;
+double totalpagar, cuentatotal;
+for (int i = 0; i <= N; i++)
+{
+    Console.WriteLine("Cuantos kilos comprara:  ");
+    kilos = int.Parse(Console.ReadLine());
+    valorkilos = 84;
+    if (kilos > 10)
+    {
+        cuentatotal = kilos * valorkilos;
+        totalpagar = cuentatotal - cuentatotal * 0.15;
+    }
+    else
+        totalpagar = kilos * valorkilos;
+    Console.WriteLine("el cliente pagara: " + totalpagar);
+
+}
+Console.ReadKey();
